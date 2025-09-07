6f624b734ba89eb8fd052cadb6d3142d173da87b
Ofir Gibly <ofir.gibly@sandisk.com>
2025-09-07 11:02:22 +0300

    fixed missing import issue in models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from .database import Base
+from datetime import datetime
 
 class Device(Base):
     __tablename__ = "devices"

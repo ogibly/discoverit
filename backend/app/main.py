ea7ba544e054916a3ebdb5f773da3b2e97d2981f
OG <ofirgibly@gmail.com>
2025-09-14 14:17:48 +0300

    restabilized the application

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,9 +5,14 @@ from .database import engine, Base
 from .routes import router
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
